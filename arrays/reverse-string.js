227e74d25cbeea2138d33f1007a36848d321c2e5
Matt <matthew.sheehan94@gmail.com>
2023-08-22 22:47:45 +0100

    implement reverse string solution

--- arrays/reverse-string.js
+++ arrays/reverse-string.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-
+    const reversedStr = []
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr.push(str[i])
+    }
+    return reversedStr.join("")
 }
 
-reverse("hello, world!")
+console.log(reverse("hello, world!"))
