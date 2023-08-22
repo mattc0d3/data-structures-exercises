9463259f39af99482fe2668e9bf5f41a3d0fad5a
Matt <matthew.sheehan94@gmail.com>
2023-08-22 23:57:22 +0100

    implement improved O(n) solution for mergeSortedArrays

--- arrays/merge-sorted-arrays.js
+++ arrays/merge-sorted-arrays.js
@@ -14,4 +14,29 @@ function mergeSortedArrays(arr1, arr2) {
     return mergedArray
 }
 
-console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]))
+function mergeSortedArrays2(arr1, arr2) {
+    const mergedArray = []
+    let arr1Item = arr1[0]
+    let arr2Item = arr2[0]
+    let i = 1
+    let j = 1
+
+    if (!arr1.length) return arr2
+    else if (!arr2.length) return arr1
+
+    while (arr1Item || arr2Item) {
+        if (!arr2Item || arr1Item < arr2Item) {
+            mergedArray.push(arr1Item)
+            arr1Item = arr1[i]
+            i++
+        } else {
+            mergedArray.push(arr2Item)
+            arr2Item = arr2[j]
+            j++
+        }
+    }
+
+    return mergedArray
+}
+
+console.log(mergeSortedArrays2([0, 3, 4, 31], [4, 6, 30]))
