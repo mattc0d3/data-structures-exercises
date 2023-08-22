fd7a0a0dde64456f1c764bfb0777c715148838cf
Matt <matthew.sheehan94@gmail.com>
2023-08-22 22:53:37 +0100

    add input checks for reverse string function

--- arrays/reverse-string.js
+++ arrays/reverse-string.js
@@ -1,4 +1,6 @@
 function reverse(str) {
+    if (!str || typeof str !== "string") return "invalid input"
+
     const reversedStr = []
     for (let i = str.length - 1; i >= 0; i--) {
         reversedStr.push(str[i])
