3f1b77e3d942de98541a1993ad51bfd88da003d4
Matt <matthew.sheehan94@gmail.com>
2023-08-22 22:54:57 +0100

    create totalIndexes variable to prevent multiple length calls

--- arrays/reverse-string.js
+++ arrays/reverse-string.js
@@ -2,7 +2,9 @@ function reverse(str) {
     if (!str || typeof str !== "string") return "invalid input"
 
     const reversedStr = []
-    for (let i = str.length - 1; i >= 0; i--) {
+    const totalIndexes = str.length - 1
+
+    for (let i = totalIndexes; i >= 0; i--) {
         reversedStr.push(str[i])
     }
     return reversedStr.join("")
