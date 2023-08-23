f77a21d1bd09943b434ca3bae44871451d741e22
Matt <matthew.sheehan94@gmail.com>
2023-08-23 16:04:18 +0100

    add slower alternative solution for first recurring char using only array structures

--- hash-tables/first-recurring-character.js
+++ hash-tables/first-recurring-character.js
@@ -1,3 +1,4 @@
+// O(n) solution
 function firstRecurringChar(arr) {
     const seenChars = {}
 
@@ -15,3 +16,25 @@ function firstRecurringChar(arr) {
 console.log(firstRecurringChar([2,5,1,2,3,5,1,2,4])) // Should return 2
 console.log(firstRecurringChar([2,1,1,2,3,5,1,2,4])) // 1
 console.log(firstRecurringChar([2,3,4,5])) // undefined
+
+// O(n2) solution
+function firstRecurringChar2(arr) {
+    const pairedIndexes = []
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                pairedIndexes.push(j)
+            }
+        }
+    }
+    if (pairedIndexes.length) {
+        const lowestPairedIndex = Math.min(...pairedIndexes)
+        return arr[lowestPairedIndex]
+    } else {
+        return undefined
+    }
+}
+
+console.log(firstRecurringChar2([2,5,5,2,3,5,1,2,4])) // Should return 5
+console.log(firstRecurringChar2([2,1,1,2,3,5,1,2,4])) // 1
+console.log(firstRecurringChar2([2,3,4,5])) // undefined
