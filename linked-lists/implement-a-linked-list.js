2810d6ce15b66993a37c0de39b13ba7714e896c3
Matt <matthew.sheehan94@gmail.com>
2023-08-24 22:02:53 +0100

    add insert method to linked list

--- linked-lists/implement-a-linked-list.js
+++ linked-lists/implement-a-linked-list.js
@@ -28,6 +28,38 @@ class LinkedList {
         this.length++
         return this
     }
+    PrintList() {
+        const array = []
+        let currentNode = this.head
+        while (currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array
+    }
+    Insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value)
+
+        }
+        if (index === 0) {
+            return this.prepend(value)
+        }
+        const newNode = new Node(value)
+        const leader = this.traverseToIndex(index - 1)
+        newNode.next = leader.next
+        leader.next = newNode
+        return this.PrintList()
+    }
+    traverseToIndex(index) {
+        let counter = 0
+        let currentNode = this.head
+        while (counter !== index) {
+            currentNode = currentNode.next
+            counter++
+        }
+        return currentNode
+    }
 }
 
 const myLinkedList = new LinkedList(5)
@@ -38,3 +70,6 @@ myLinkedList.append(7)
 console.log(myLinkedList)
 myLinkedList.prepend(4)
 console.log(myLinkedList)
+console.log(myLinkedList.PrintList())
+myLinkedList.Insert(3, 11)
+console.log(myLinkedList.PrintList())
