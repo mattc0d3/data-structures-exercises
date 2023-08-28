101cd19e557bf48734a0205006098380227384be
Matt <matthew.sheehan94@gmail.com>
2023-08-28 18:53:42 +0100

    add first pass at implementing a stack

---
+++ stacks/implement-a-stack.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(value) {
+        this.value = value,
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null,
+        this.bottom = null,
+        this.length = 0
+    }
+    peek() {
+        return this.top
+    }
+    push(value) {
+        const newNode = new Node(value)
+        newNode.next = this.top
+        this.top = newNode
+        this.length++
+    }
+    pop() {
+        this.top = this.top.next
+        this.length--
+    }
+    isEmpty() {
+        return this.length === 0
+    }
+}
+
+const myStack = new Stack()
+
+myStack.push(1)
+console.log(myStack)
+myStack.push(2)
+myStack.push(3)
+console.log(myStack.peek())
+console.log(myStack)
+myStack.pop()
+console.log(myStack.peek())
+console.log(myStack)
