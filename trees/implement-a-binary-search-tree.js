306270b523134ac0e02fcecaf15fc0921a635e65
Matt <matthew.sheehan94@gmail.com>
2023-08-29 10:49:34 +0100

    first pass at implementing binary search tree

---
+++ trees/implement-a-binary-search-tree.js
@@ -0,0 +1,61 @@
+class Node {
+    constructor(value) {
+        this.value = value,
+        this.left = null,
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+    insert(value) {
+        const newNode = new Node(value)
+        if (this.root === null) this.root = newNode
+        else {
+            let currentNode = this.root
+            while (currentNode !== null) {
+                if (newNode.value === currentNode.value) return "Value already exists"
+                else if (newNode.value < currentNode.value) {
+                    if (currentNode.left === null) currentNode.left = newNode
+                    currentNode = currentNode.left
+                }
+                else {
+                    if (currentNode.right === null) currentNode.right = newNode
+                    currentNode = currentNode.right
+                }
+            }
+            currentNode = newNode
+            return this
+        }
+    }
+    lookup(value) {
+        let currentNode = this.root
+        while (currentNode !== null) {
+            if (currentNode.value === value) return true
+            if (value < currentNode.value) currentNode = currentNode.left
+            else currentNode = currentNode.right
+        }
+        return false
+    }
+}
+
+const myTree = new BinarySearchTree()
+myTree.insert(9)
+myTree.insert(4)
+myTree.insert(6)
+myTree.insert(20)
+myTree.insert(170)
+myTree.insert(15)
+myTree.insert(1)
+console.log(JSON.stringify(traverse(myTree.root)))
+console.log(myTree.lookup(15))
+console.log(myTree.lookup(16))
+
+function traverse(node) {
+    const tree = { value: node.value }
+    tree.left = node.left === null ? null : traverse(node.left)
+    tree.right = node.right === null ? null : traverse(node.right)
+    return tree
+}
