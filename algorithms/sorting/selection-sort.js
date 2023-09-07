9b222faf00877d3a32e92cc17684a1346fe81ac0
Matt <matthew.sheehan94@gmail.com>
2023-09-07 12:24:19 +0100

    simplify selection sort solution

--- algorithms/sorting/selection-sort.js
+++ algorithms/sorting/selection-sort.js
@@ -2,16 +2,15 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(array) {
     for (let i = 0; i < array.length - 1; i++) {
-        let smallestNum = array[i]
-        let swapIndex = i
+        let smallestNum = i
         for (let j = i + 1; j < array.length; j++) {
-            if (smallestNum > array[j]) {
-                smallestNum = array[j]
-                swapIndex = j
+            if (array[smallestNum] > array[j]) {
+                smallestNum = j
             }
         }
-        array[swapIndex] = array[i]
-        array[i] = smallestNum
+        placeholder = array[i]
+        array[i] = array[smallestNum]
+        array[smallestNum] = placeholder
     }
 }
 
