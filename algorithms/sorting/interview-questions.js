b013af1d400626d4b008faace379555ec76c4dff
Matt <matthew.sheehan94@gmail.com>
2023-09-07 17:57:26 +0100

    add answers to sorting interview questions

---
+++ algorithms/sorting/interview-questions.js
@@ -0,0 +1,15 @@
+//#1 - Sort 10 schools around your house by distance: Insertion
+
+//#2 - eBay sorts listings by the current Bid amount: Radix or Counting
+
+//#3 - Sport scores on ESPN: Quick
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data: Merge
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews: Insertion
+
+//#6 - Temperature Records for the past 50 years in Canada: Radix or Counting if no decimal places, Quick for more accurate data
+
+//#7 - Large user name database needs to be sorted. Data is very random: Merge if memory not an issue, Quick if worst case not an issue
+
+//#8 - You want to teach sorting for the first time: Bubble or Selection
