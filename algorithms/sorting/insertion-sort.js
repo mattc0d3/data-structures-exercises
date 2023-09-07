57973f9ab28309ccc62a32105b3851f5bb64ffa6
Matt <matthew.sheehan94@gmail.com>
2023-09-07 13:25:27 +0100

    add alternative insertion sort solution

--- algorithms/sorting/insertion-sort.js
+++ algorithms/sorting/insertion-sort.js
@@ -23,3 +23,22 @@ function insertionSort(array) {
 
 insertionSort(numbers);
 console.log(numbers);
+
+const numbers2 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort2(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < array[0]) {
+            array.unshift(array.splice(i, 1)[0])
+        } else {
+            for (let j = 1; j < i; j++) {
+                if (array[i] > array[j - 1] && array[i] < array[j]) {
+                    array.splice(1, 0, array.splice(i, 1)[0])
+                }
+            }
+        }
+    }
+}
+
+insertionSort2(numbers2);
+console.log(numbers2);
